d7fc8dc9900fd0ec5f8b9b4c8adb9979cc85e1e9
Marco Gosselink <marco@famgosselink.nl>
2022-09-01 20:22:14 +0200

    Added DOCS

--- upload_image_github.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-  --rm \
-  --privileged \
-  -v ~/.docker:/root/.docker \
-  homeassistant/amd64-builder \
-  --all \
-  -t addon-vantagepro2mqtt \
-  -r https://github.com/MarcoGos/ha-addons \
-  -b beta
