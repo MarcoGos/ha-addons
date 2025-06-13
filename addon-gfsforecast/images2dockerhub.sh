1c64906bbec7afb07a57e15aa0376ea3eeab387d
Marco Gosselink <marco@famosselink.nl>
2025-06-13 21:31:31 +0200

    Deprecated vantagepro2mqtt and gfsforecast

--- addon-gfsforecast/images2dockerhub.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-  --rm \
-  --privileged \
-  -v ~/.docker:/root/.docker \
-  homeassistant/amd64-builder \
-  --all \
-  -t addon-gfsforecast \
-  -r https://github.com/MarcoGos/ha-addons \
-  -b beta
